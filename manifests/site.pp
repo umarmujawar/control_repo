4640d208418769969f6eef8c5ac9d49d59ab8914
lastusrnameon3 <34868770+lastusrnameon3@users.noreply.github.com>
2018-09-03 19:54:06 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,7 @@
-node default{
-  file '/root/README':
-    ensure => exists,
+node default {
+  file { '/root/README':
+    ensure => file,
+    content => 'This is a readme',
+    owner   => 'root',
   }
 }

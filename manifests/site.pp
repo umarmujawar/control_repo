7b7be61316ebb9e5ce1a777d1e2aa78a7e2e2a82
lastusrnameon3 <34868770+lastusrnameon3@users.noreply.github.com>
2018-09-05 23:01:34 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,3 @@
-node default {
-  file { '/root/README':
-    ensure  =>  file,
-    content =>  'This is a readme',
-    owner   =>  'root',
-    mode    =>   644,
-  }
-}
-
 node 'agent1.test.com' {
      package { 'httpd':
          ensure  => "installed",

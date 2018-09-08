b18523a37c5aac95d54d78d296739fad9f8144b2
lastusrnameon3 <34868770+lastusrnameon3@users.noreply.github.com>
2018-09-08 18:02:54 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,11 @@
 #define node
 node 'agent1.test.com' {
-  
+  {
+   #Config file for NTPd
+    file { "/etc/ntp.conf":
+    ensure  => "present",
+    content => "server 0.centos.pool.ntp.org iburst\n",
+  }
 }
 
 #Define Default

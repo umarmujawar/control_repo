47725ca230258d5ba944bf0c4621c191cc213771
yogeshraheja <yogesh.10d@gmail.com>
2018-09-09 14:33:17 +0530

    Fixed code

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,10 @@
 
 #Define Default
-  node 'default', 'agent1.test.com' {
-  #Config file for NTPd
-    file { "/etc/hi.txt":
-      ensure  => "present",
-      content => "hi there",
-    }
+node "agent1.test.com" {
+
+#Config file for NTPd
+  file { "/etc/hi.txt":
+    ensure  => "present",
+    content => "hi there",
   }
+}

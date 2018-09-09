a98abec253e7b9126fd8e95d11f41f8a8ad75a5e
lastusrnameon3 <34868770+lastusrnameon3@users.noreply.github.com>
2018-09-08 20:33:01 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,16 +3,18 @@ node 'agent1.test.com' {
   {
    #Config file for NTPd
     file { "/etc/ntp.conf":
-    ensure  => "present",
-    content => "server 0.centos.pool.ntp.org iburst\n",
+      ensure  => "present",
+      content => "server 0.centos.pool.ntp.org iburst\n",
+    }
   }
 }
 
 #Define Default
-node 'default' {
- #Config file for NTPd
-  file { "/etc/ntp.conf":
-    ensure  => "present",
-    content => "server 0.centos.pool.ntp.org iburst\n",
-}
+  node 'default' {
+  #Config file for NTPd
+    file { "/etc/hi.txt":
+      ensure  => "present",
+      content => "hi there",
+    }
+  }
 }
